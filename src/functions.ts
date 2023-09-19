47e537ce28f29e8561da743195c78d78eec27d93
Rafael Pacheco <rafaelpache64@outlook.com>
2023-09-19 18:03:31 -0400

    I programmed the creation of new todo items.
    Also added id to them.

--- src/functions.ts
+++ src/functions.ts
@@ -1,4 +1,10 @@
-export function setDarkTheme(darkTheme: boolean, loadingPage: boolean) {
+import { Todo } from './types';
+import { v4 as uuidv4 } from 'uuid';
+
+export function setDarkTheme(
+  darkTheme: boolean,
+  loadingPage: boolean
+): boolean {
   const body = document.querySelector('[data-body]');
   const header = document.querySelector('[data-header]');
   const appSection = document.querySelector('[data-app-section]');
@@ -8,10 +14,9 @@ export function setDarkTheme(darkTheme: boolean, loadingPage: boolean) {
     '.btn-with-icon, .btn-without-icon'
   );
 
-  if (!loadingPage) {
-    darkTheme = !darkTheme;
-    localStorage.setItem('darkTheme', `${darkTheme}`);
-  }
+  darkTheme = loadingPage ? darkTheme : !darkTheme;
+
+  localStorage.setItem('darkTheme', `${darkTheme}`);
 
   body?.setAttribute(`data-dark-theme`, `${darkTheme}`);
   header?.setAttribute(`data-dark-theme`, `${darkTheme}`);
@@ -22,12 +27,82 @@ export function setDarkTheme(darkTheme: boolean, loadingPage: boolean) {
   allBtns.forEach((btn) => {
     btn?.setAttribute(`data-dark-theme`, `${darkTheme}`);
   });
+
+  return darkTheme;
 }
 
-export function getLocalStorageKeys(): { darkTheme: string | null } {
+export function getLocalStorageKeys(): {
+  darkTheme: string | null;
+} {
   const keysObj: { darkTheme: string | null } = {
     darkTheme: localStorage.getItem('darkTheme'),
   };
 
   return keysObj;
+}
+
+export function handleSubmitFormAddTodo(
+  valueInput: string | undefined,
+  allToDos: Todo[],
+  darkTheme: boolean
+): Todo[] {
+  if (!valueInput) return allToDos;
+
+  const newTodo: Todo = {
+    id: `${uuidv4()}`,
+    text: `${valueInput}`,
+    checked: false,
+  };
+
+  const newAllToDos: Todo[] = [newTodo, ...allToDos];
+
+  createTodoItems(newAllToDos, darkTheme);
+
+  return newAllToDos;
+}
+
+export function createTodoItems(
+  todoItemsArray: Todo[],
+  darkTheme: boolean
+): void {
+  todoItemsArray.map((todoObjItem) => {
+    const previousTodoElement = document.getElementById(`${todoObjItem.id}`);
+
+    previousTodoElement?.remove();
+
+    createSingleTodoItem(todoObjItem);
+  });
+}
+
+export function createSingleTodoItem(todoObjItem: Todo) {
+  const todoListElement = document.querySelector('[data-todo-list]');
+
+  const liElement = document.createElement('li');
+  const btnCheckElement = document.createElement('button');
+  const btnCheckElementIcon = document.createElement('i');
+  const textLiElement = document.createElement('p');
+  const btnDeleteElement = document.createElement('button');
+  const btnDeleteElementIcon = document.createElement('i');
+
+  liElement.id = todoObjItem.id;
+  liElement.classList.add('todo-list-item');
+
+  btnCheckElement.classList.add('btn-with-icon', 'btn-check-todo');
+  btnCheckElement.setAttribute('data-checked-todo', 'false');
+  btnCheckElementIcon.classList.add('fa-solid', 'btn-with-icon-i');
+
+  textLiElement.classList.add('todo-list-item__p');
+  textLiElement.textContent = `${todoObjItem.text}`;
+
+  btnDeleteElement.classList.add('btn-with-icon', 'btn-remove-todo');
+  btnDeleteElementIcon.classList.add('fa-solid', 'fa-trash');
+
+  liElement.append(btnCheckElement);
+  liElement.append(textLiElement);
+  liElement.append(btnDeleteElement);
+
+  btnCheckElement.append(btnCheckElementIcon);
+  btnDeleteElement.append(btnDeleteElementIcon);
+
+  todoListElement?.append(liElement);
 }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,30 @@
-import { getLocalStorageKeys, setDarkTheme } from './functions';
+import {
+  getLocalStorageKeys,
+  handleSubmitFormAddTodo,
+  setDarkTheme,
+} from './functions';
+import { Todo } from './types';
 
-const changeThemeBtn = document.querySelector('[data-change-theme-button]');
 let darkTheme: boolean = getLocalStorageKeys().darkTheme === 'true';
+
+let allToDos: Todo[] = [];
 
 setDarkTheme(darkTheme, true);
 
+const changeThemeBtn = document.querySelector('[data-change-theme-button]');
+
 changeThemeBtn?.addEventListener('click', () => {
-  setDarkTheme(darkTheme, false);
+  darkTheme = setDarkTheme(darkTheme, false);
+});
+
+const formAddToDo = document.querySelector('[data-form-add-todo]');
+
+formAddToDo?.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const target: HTMLInputElement | null = document.querySelector(
+    '[data-input-add-todo]'
+  );
+
+  allToDos = handleSubmitFormAddTodo(target?.value, allToDos, darkTheme);
 });
